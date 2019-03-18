7c5a63f433196beebe33ee5cabd9df58736fc79f
黃哲芬 <annie101101046@gmail.com>
2019-03-18 17:14:23 +0800

    updates

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,3 +1,7 @@
+//FB 的順序要調整
+//先進入註冊頁面 -> 註冊成功或登入成功才會進 profile 頁面
+//下次再點 profile 頁面會讀 user profile 的一個 id 值去確認
+
 app.init = function () {
 	app.cart.init();
 	app.fb.statusChangeCallback = app.initProfile;
@@ -30,19 +34,31 @@ app.showProfile = function (data) {
 };
 window.addEventListener("DOMContentLoaded", app.init);
 
-//讀 localstorage 的資料印出來
-if (typeof (Storage) !== "undefined") {
-	console.log(localStorage);
-	var username = document.getElementById('load_name');
-	var email = document.getElementById('load_email');
-	var picture = document.getElementById('profile_picture');
-	var data = localStorage.getItem('personalData');
-	data = JSON.parse(data);
+// //讀從後端拿出來的資料然後印出來
+// if (typeof (Storage) !== "undefined") {
+// 	console.log(localStorage);
+// 	var username = document.getElementById('load_name');
+// 	var email = document.getElementById('load_email');
+// 	var picture = document.getElementById('profile_picture');
+// 	var data = localStorage.getItem('personalData');
+// 	data = JSON.parse(data);
 
 
-	username.innerHTML = data.usename;
-	email.innerHTML = data.userEmail;
-	picture.innerHTML = data.userPicture;
-} else {
-	// No web storage Support.
+// 	username.innerHTML = data.usename;
+// 	email.innerHTML = data.userEmail;
+// 	picture.innerHTML = data.userPicture;
+// } else {
+// 	// No web storage Support.
+// }
+
+
+//拿 user 的 profile 印出來
+function profile() {
+	const url = 'https://api.appworks-school.tw/api/1.0/user/profile';
+	const data = {
+		"name": document.getElementById('login-name').value,
+		"email": document.getElementById('login-account').value,
+		"password": document.getElementById('login-password').value
+	};
+
 }
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,5 +1,5 @@
 //sign in
-//一般會員登入
+//一般會員註冊
 //輸入 email, password 後點擊登入按鈕，判斷是否有此人，若有此人跳到 profile 頁，讀出姓名、email
 
 function signup() {
@@ -36,18 +36,18 @@ function signup() {
 						userPicture: result.data.user.picture
 					}
 
-					console.log(personalData);
-					//存到 localStorage
-					//把 result 的資料找到，然後拿出要哪些資料，整理過後塞到 localStorage
-					// set on load for testing
-					localStorage.setItem('personalData', JSON.stringify(personalData));
+					//讓後端拿到資料，然後跳轉到 profile 頁面
+
+					// console.log(personalData);
+					// //存到 localStorage
+					// //把 result 的資料找到，然後拿出要哪些資料，整理過後塞到 localStorage
+					// // set on load for testing
+					// localStorage.setItem('personalData', JSON.stringify(personalData));
 					window.location = "profile.html";
 				});
 			} else {
 				throw ('fetch error')
 			}
-
-
 			// 輸出成 json
 		})
 		.catch(err => console.log('error', err));
@@ -57,108 +57,50 @@ let signUpButton = document.getElementById('signUpButton');
 signUpButton.addEventListener("click", signup);
 
 
+//一般會員登入
+function signin() {
+	const url = 'https://api.appworks-school.tw/api/1.0/user/signin';
+	const data = {
+		"email": document.getElementById('loginAccount').value,
+		"password": document.getElementById('loginPassword').value,
+		"provider": "native"
+	};
 
+	// Default options are marked with *
+	return fetch(url, {
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data), // must match 'Content-Type' header
+			method: 'POST', // *GET, POST, PUT, DELETE, etc.
+		})
+		.then(response => {
 
+			const {
+				status
+			} = response;
 
-//getDataresponse(name, email)
-//data.user.name
-//data.user.email
+			// const status = response.status;
 
+			if (status) {
+				response.json().then(result => {
+					console.log('result', result)
 
-// //facebook 登入
-// //點擊 facebook 登入按鈕，判斷是否有此人，若有此人跳到 profile 頁，讀出姓名、email & 大頭貼
-// app.init = function () {
-// 	app.cart.init();
-// 	app.fb.statusChangeCallback = app.initProfile;
-// }; app.initProfile = function () {
-// 	if (app.state.auth === null) {
-// 		window.location = "./";
-// 	}
-// 	app.fb.getProfile().then(function (data) {
-// 		app.showProfile(data);
-// 	}).catch(function (error) {
-// 		console.log("Facebook Error", error);
-// 	});
-// }; app.showProfile = function (data) {
-// 	app.get("#profile-picture").src = "https://graph.facebook.com/" + data.id + "/picture/?width=200";
-// 	let details = app.get("#profile-details");
-// 	app.createElement("div", {
-// 		atrs: {
-// 			className: "name",
-// 			textContent: data.name
-// 		}
-// 	}, details);
-// 	app.createElement("div", {
-// 		atrs: {
-// 			className: "email",
-// 			textContent: data.email
-// 		}
-// 	}, details);
-// }; window.addEventListener("DOMContentLoaded", app.init);
+					let userData = {
+						usename: result.data.user.name,
+						userEmail: result.data.user.email,
+						userPicture: result.data.user.picture
+					}
+					//讓後端拿到資料確認，然後跳轉到 profile 頁面
+					window.location = "profile.html";
+				});
+			} else {
+				throw ('fetch error')
+			}
+			// 輸出成 json
+		})
+		.catch(err => console.log('error', err));
+}
 
-// //create account
-// //一般會員註冊
-// //輸入 email, password, 姓名後把資料傳給後端
-// const url = 'https://api.appworks-school.tw/api/1.0/user/signup';
-// const data = {
-// 	"name": "",
-// 	"email": "",
-// 	"password": ""
-// };
-
-// const otherPram = {
-// 		"content-type": "application/json; charset = UTF-8"
-// 	},
-// 	body: Data,
-// 	method: "POST"
-
-// fetch(Url, otherPram)
-// .then(data => {
-// 	return data.json()
-// })
-// .then(res => {
-// 	console.log(res)
-// })
-// .catch(error => console.log(error))
-
-// //可以自己上傳大頭貼照
-// <
-// form action = "/api/1.0/admin/avatar"
-// method = "post"
-// enctype = "multipart/form-data" >
-// <
-// input type = "file"
-// name = "avatar" / >
-// <
-// /form>
-
-
-// //facebook 註冊
-// app.init = function () {
-// 	app.cart.init();
-// 	app.fb.statusChangeCallback = app.initProfile;
-// }; app.initProfile = function () {
-// 	if (app.state.auth === null) {
-// 		window.location = "./";
-// 	}
-// 	app.fb.getProfile().then(function (data) {
-// 		app.showProfile(data);
-// 	}).catch(function (error) {
-// 		console.log("Facebook Error", error);
-// 	});
-// }; app.showProfile = function (data) {
-// 	app.get("#profile-picture").src = "https://graph.facebook.com/" + data.id + "/picture/?width=200";
-// 	let details = app.get("#profile-details");
-// 	app.createElement("div", {
-// 		atrs: {
-// 			className: "name",
-// 			textContent: data.name
-// 		}
-// 	}, details);
-// 	app.createElement("div", {
-// 		atrs: {
-// 			className: "email",
-// 			textContent: data.email
-// 		}
-// 	}, details);
-// }; window.addEventListener("DOMContentLoaded", app.init);
+let sign1 = document.getElementById('sign1');
+sign1.addEventListener("click", signin);
